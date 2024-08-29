3667b9d596d0c06b86e7a34118db3a8913bf457b
MEHDI-AHMADY <metti799@gmail.com>
2024-08-29 03:13:27 -0700

    fetchData from TMdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+const API_KEY = process.env.API_KEY;
+import axios from "axios";
+import Results from "@/components/Results/Results";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await axios.get(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en_US&page=1`
+  );
+
+  if(res.statusText !== "OK") {
+    throw new Error("Failed to Fetch Data.")
+  }
+
+  const results = await res.data.results;
+  
   return (
     <div>
-      
+      <Results results={results} />
     </div>
-  )
+  );
 }
