fa3f4a0396c9c58ad86be16e1c9b98ee3e8ba2ac
MEHDI-AHMADY <metti799@gmail.com>
2024-08-29 00:45:42 -0700

    feat : add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import { TiHome } from "react-icons/ti";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import MenuItem from "../MenuItem/MenuItem";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <div className="flex gap-4">
+        <MenuItem title="home" address="/" Icon={TiHome} />
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <Link href={"/"} className="flex gap-1 items-center">
+        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg ">IMdb</span>
+        <span className="hidden sm:inline text-xl">Clone</span>
+      </Link>
+    </div>
+  );
+}
