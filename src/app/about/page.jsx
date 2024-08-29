1c817fbe7a46ade2148510c99b5a532b8ab37557
MEHDI-AHMADY <metti799@gmail.com>
2024-08-29 02:07:22 -0700

    feat : add navbar component

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl text-amber-600 font-medium">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+        quas illum atque velit repudiandae iste eos dolor, ipsum quia quasi
+        praesentium at dignissimos dolores delectus neque perspiciatis aliquam
+        unde possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+        quas illum atque velit repudiandae iste eos dolor, ipsum quia quasi
+        praesentium at dignissimos dolores delectus neque perspiciatis aliquam
+        unde possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+        quas illum atque velit repudiandae iste eos dolor, ipsum quia quasi
+        praesentium at dignissimos dolores delectus neque perspiciatis aliquam
+        unde possimus.
+      </p>
+    </div>
+  );
+}
