bb39ad8b60b8cbaad7998188bb9c608b73f43bdc
MEHDI-AHMADY <metti799@gmail.com>
2024-08-29 01:38:30 -0700

    feat : add dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { TiHome } from "react-icons/ti";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import MenuItem from "../MenuItem/MenuItem";
 import Link from "next/link";
+import DarkModeSwitch from "../DarkModeSwitch/DarkModeSwitch";
 
 export default function Header() {
   return (
@@ -10,10 +11,15 @@ export default function Header() {
         <MenuItem title="home" address="/" Icon={TiHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={"/"} className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg ">IMdb</span>
-        <span className="hidden sm:inline text-xl">Clone</span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkModeSwitch />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg ">
+            IMdb
+          </span>
+          <span className="hidden sm:inline text-xl">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
