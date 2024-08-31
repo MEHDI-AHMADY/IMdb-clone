42caec2a6766aacba2079d9215c86487bc966835
MEHDI-AHMADY <metti799@gmail.com>
2024-08-31 00:15:07 -0700

    feat : add movie page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 const API_KEY = process.env.API_KEY;
 import axios from "axios";
 import Results from "@/components/Results/Results";
-import { Suspense } from "react";
-import Loading from "./loading";
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
@@ -10,8 +8,9 @@ export default async function Home({ searchParams }) {
   const res = await axios.get(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en_US&page=1`
+    }?api_key=${API_KEY}&language=en_US&page=7`
   );
+
 
   if (res.statusText !== "OK") {
     throw new Error("Failed to Fetch Data.");
@@ -20,10 +19,8 @@ export default async function Home({ searchParams }) {
   const results = await res.data.results;
 
   return (
-    <Suspense fallback={<Loading />}>
       <div>
         <Results results={results} />
       </div>
-    </Suspense>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,60 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { FiThumbsUp } from "react-icons/fi";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-export default function Card({ result }) {
+const items = {
+  hidden: (i) => ({
+    opacity: 0,
+    y: i % 2 === 0 ? 20 : -20,
+  }),
+  show: (i) => ({
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1.3, delay: i * 0.3 },
+  }),
+};
+
+export default function Card({ result, index }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+    <motion.div
+      initial="hidden"
+      animate="show"
+      variants={items}
+      whileHover={{ scale: 1.05 }}
+      custom={index}
+      className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200"
+    >
       <Link href={`/movie/${result.id}`}>
+        {isLoading && (
+          <img
+            src="/images/loading.png"
+            style={{
+              width: "100%",
+              height: "auto",
+              aspectRatio: "1.78 / 1",
+              objectFit: "cover",
+            }}
+          />
+        )}
         <Image
+          priority
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster
           }`}
           width={500}
           height={300}
-          className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
+          alt={result.title || result.name}
+          className={`sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300 ${
+            isLoading ? "hidden" : "block"
+          }`}
+          onLoadingComplete={() => setIsLoading(false)}
         />
+
         <div className="p-2">
           <p className="line-clamp-2 font-medium">{result.overview}</p>
           <h2 className="text-lg font-bold truncate">
@@ -26,6 +67,6 @@ export default function Card({ result }) {
           </p>
         </div>
       </Link>
-    </div>
+    </motion.div>
   );
 }
